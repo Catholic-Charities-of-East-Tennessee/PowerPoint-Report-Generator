20503989a91ebc8d4e673a8c34b944f1beede608
Joey Borrelli <joeyjrelli3824@gmail.com>
2024-12-30 20:21:19 -0500

    Fixed bug so that last chart becomes a slide

--- app/CSV_Interpreter.py
+++ app/CSV_Interpreter.py
@@ -17,68 +17,24 @@ def test(file, cli_instance):
 
             # loop through each row in the file (the row is a list of values)
             for row in reader:
+
                 # if the row begins with text & every value after is empty, then a new chart is being made
                 if row[0] != '' and all(element == '' for element in row[1:]):
-                    # if this isn't the first time, then we need to create a slide with the date
+
+                    # if this isn't the first iteration, then we need to create a slide with the previous data
                     if title is not None:
-                        # Clean up data before sending off to pptx Generator
-                        print("\nDefault slide_rows")
-                        for r in slide_rows:
-                            print(r)
-
-                        # Remove first empty column. if there is an element in the first column, move it to the next column
-                        slide_rows = [
-                            [row[0]] + row[2:] if row[0] != '' else row[1:]
-                            for row in slide_rows
-                        ]
-                        print("\nremove first empty column")
-                        for r in slide_rows:
-                            print(r)
-
-                        # make all rows same length
-                        # Find chart length
-                        chart_length = 0
-                        # loop through each row
-                        for sRow in slide_rows:
-                            # loop through each element in a row
-                            for i in range(len(sRow)):
-                                if sRow[i] != '' and i > chart_length:
-                                    chart_length = i
-                        # account for list starting at 0
-                        chart_length = chart_length + 1
-                        # set new lines to the proper length
-                        if len(slide_rows[1]) - chart_length != 0: # this conditional is because the slide with the most columns will be deleted using this algorithm
-                            slide_rows = [s_row[:-(len(sRow) - chart_length)] for s_row in slide_rows]
-                        print("\nmake all rows same length")
-                        print("Chart length: " + str(chart_length))  # check if chart length worked
-                        for r in slide_rows:
-                            print(r)
-
-                        # remove any instances of 'Count'
-                        for i in range(len(slide_rows)):  # loop through columns
-                            for j in range(len(slide_rows[i])):  # loop through rows
-                                if slide_rows[i][j] == 'Count':
-                                    slide_rows[i][j] = ''
-                        print("\nremove instances of count")
-                        for r in slide_rows:
-                            print(r)
-
-                        # remove any empty rows
-                        slide_rows = [sublist for sublist in slide_rows if not all(element == '' for element in sublist)]
-                        print("\nremove emtpy rows")
-                        for r in slide_rows:
-                            print(r)
-
-                        # create slide off data
-                        pptx_generator.create_Table_Slide(title, slide_rows, chart_length, len(slide_rows))
-
-                        # clear rows
+                        # create  slide
+                        create_slide(slide_rows, title, pptx_generator)
+                        # clear the slide_rows array so that we can make a new slide
                         slide_rows = []
                     # set slide/table title value
                     title = row[0]
                 # we are in the middle of a chart
                 else:
                     slide_rows.append(row)
+
+            # create a slide for the last chart
+            create_slide(slide_rows, title, pptx_generator)
             # save presentation
             pptx_generator.save_Presentation()
     except FileNotFoundError:
@@ -95,3 +51,38 @@ def stats_report(file, cli_instance):
 
 def stats_report_with_hope_kitchen(file, cli_instance):
     print("Starting interpreter...")
+
+def create_slide(chart, title, pptx_generator):
+    # Clean up data before sending off to pptx Generator
+    # Remove first empty column. if there is an element in the first column, move it to the next column
+    chart = [
+        [row[0]] + row[2:] if row[0] != '' else row[1:]
+        for row in chart
+    ]
+
+    # make all rows same length
+    # Find chart length
+    chart_length = 0
+    # loop through each row
+    for sRow in chart:
+        # loop through each element in a row
+        for i in range(len(sRow)):
+            if sRow[i] != '' and i > chart_length:
+                chart_length = i
+    # account for list starting at 0
+    chart_length = chart_length + 1
+    # set new lines to the proper length
+    if len(chart[1]) - chart_length != 0:  # this conditional is because the slide with the most columns will be deleted using this algorithm
+        chart = [s_row[:-(len(s_row) - chart_length)] for s_row in chart]
+
+    # remove any instances of 'Count'
+    for i in range(len(chart)):  # loop through columns
+        for j in range(len(chart[i])):  # loop through rows
+            if chart[i][j] == 'Count' or chart[i][j] == 'count':
+                chart[i][j] = ''
+
+    # remove any empty rows
+    chart = [sublist for sublist in chart if not all(element == '' for element in sublist)]
+
+    # create slide off data
+    pptx_generator.create_Table_Slide(title, chart, chart_length, len(chart))
