33770c6e55fae76bd251de81f6f08fcefcd5f451
Joey Borrelli <joeyjrelli3824@gmail.com>
2025-01-23 09:23:17 -0600

    More work on the data puller

--- app/data_puller.py
+++ app/data_puller.py
@@ -8,29 +8,53 @@ Anno:       Anno Domini 2025
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import BOT_DATA
 import time
+import pyotp
 
 
 def pull_data():
     service = Service(executable_path = "chromedriver.exe")
     driver = webdriver.Chrome(service = service)
 
+    # Open up website
     driver.get("https://www.vistashare.com/ot2/security/login/")
 
+    # send username & password and click login
     username_element = driver.find_element(By.NAME, "__ac_name")
+    username_element.clear()
     username_element.send_keys(BOT_DATA.bot_username)
 
     password_element = driver.find_element(By.NAME, "__ac_password")
+    password_element.clear()
     password_element.send_keys(BOT_DATA.bot_password)
 
     login_element = driver.find_element(By.NAME, "login")
     login_element.click()
 
-    # deal with 2fa
+    try:
+        # find the captcha element
+        captcha_element = driver.find_element(By.NAME, "captcha")
+        # if this element is found then we need to add in a 20-second sleep for someone to manually complete it.
+        time.sleep(20)
+    except NoSuchElementException:
+        print("No Capcha")
 
+    # generate 2fa (2-factor authentication) otp (one time password)
+    totp = pyotp.TOTP(BOT_DATA.otp_secret)
 
+    # Wait one second for the page to load
+    time.sleep(1)
 
-    time.sleep(10)
+    # send the otp
+    otp_element = driver.find_element(By.NAME, "security_code")
+    otp_element.clear()
+    otp_element.send_keys(totp.now())
+
+    verify_element = driver.find_element(By.NAME, "verify")
+    verify_element.click()
+
+    time.sleep(20)
 
     driver.quit()
